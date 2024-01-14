b253c508cc567f5638211e244f4b07dceac6ebf9
HongHaiTran <honghai31071998@gmail.com>
2024-01-14 23:48:36 +0700

    study git2

--- selenium/javaSDET/Topic_01_Demo.java
+++ selenium/javaSDET/Topic_01_Demo.java
@@ -3,6 +3,9 @@ package javaSDET;
 public class Topic_01_Demo {
 
     public static void main(String[] args){
+
         System.out.println("Hello world");
+        System.out.println("Hello world 2");
+
     }
 }
