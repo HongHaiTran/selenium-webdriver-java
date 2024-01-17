b4552c2367234d6cbc78524aa0b64deb61d219d3
HongHaiTran <honghai31071998@gmail.com>
2024-01-17 22:54:06 +0700

    commit 12

---
+++ selenium/javaSDET/Topic_03.java
@@ -0,0 +1,10 @@
+package javaSDET;
+
+public class Topic_03 {
+    public static void main(String[] args){
+
+        System.out.println("Hello world");
+        System.out.println("Hello world 2");
+
+    }
+}
