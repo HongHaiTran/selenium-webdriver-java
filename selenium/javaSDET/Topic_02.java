95c2e927313e33b0b029941f2785dacc9bc87d5e
Admin <honghai31071998@gmail.com>
2024-03-13 23:13:00 +0700

    Test clone

--- selenium/javaSDET/Topic_02.java
+++ selenium/javaSDET/Topic_02.java
@@ -6,7 +6,7 @@ public class Topic_02 {
 
         System.out.println("Hello world");
         System.out.println("Try again");
-        System.out.println("Hello world 2   nn");
+        System.out.println("Hello world 2     nn");
 
     }
 }
